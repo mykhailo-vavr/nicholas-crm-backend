2c7564ad7cd8d35e2ed19cfd320bbf34651d7a7a
Mykhailo <vavr.mykhailo@gmail.com>
2024-11-12 02:37:30 +0200

    feat: configure new version of eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import globals from 'globals';
+import jsPlugin from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['dist', 'eslint.config.mjs'] },
+  { languageOptions: { globals: globals.node } },
+  jsPlugin.configs.recommended,
+  ...tseslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ['{src,test}/**/*.ts'],
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prefer-destructuring': [
+        'error',
+        {
+          array: false,
+          object: true,
+        },
+        {
+          enforceForRenamedProperties: false,
+        },
+      ],
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    },
+  },
+  prettierPlugin,
+];
