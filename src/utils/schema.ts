f9329c75f9612bfed43ecb696e8fa764b0dddf3c
Mykhailo <vavr.mykhailo@gmail.com>
2025-07-26 23:33:54 +0300

    refactor: wip

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,7 +1,5 @@
 import { z } from 'zod';
 
-export const phoneSchema = z.string().regex(/^380(50|63|66|67|68|73|75|77|89|93|95|96|97|98|99)\d{7}$/);
-
 export const stringSchema = z.string().trim().min(1);
 
 export const uaStringSchema = z
@@ -13,6 +11,8 @@ export const uaStringSchema = z
 export const firstNameSchema = uaStringSchema.max(20);
 
 export const lastNameSchema = uaStringSchema.max(20);
+
+export const phoneSchema = z.string().regex(/^380(50|63|66|67|68|73|75|77|89|93|95|96|97|98|99)\d{7}$/);
 
 export const emailSchema = z.string().email().max(100);
 
